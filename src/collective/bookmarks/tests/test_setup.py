ea96dfca0010e28ae0876ae06b7ff6479e7cbae3
Jens W. Klein <jk@kleinundpartner.at>
2020-04-08 12:06:54 +0200

    isort

--- src/collective/bookmarks/tests/test_setup.py
+++ src/collective/bookmarks/tests/test_setup.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.bookmarks.testing import (
-    COLLECTIVE_BOOKMARKS_INTEGRATION_TESTING,
-)  # noqa: E501
+from collective.bookmarks.testing import COLLECTIVE_BOOKMARKS_INTEGRATION_TESTING
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
