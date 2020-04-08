72a26712615535bc6119345b02ed96cebbe61831
Jens W. Klein <jk@kleinundpartner.at>
2020-04-08 11:21:37 +0200

    "Create addon: collective.bookmarks"

---
+++ docs/index.rst
@@ -0,0 +1,5 @@
+====================
+collective.bookmarks
+====================
+
+User documentation
---
+++ src/collective/bookmarks/testing.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import collective.bookmarks
+
+
+class CollectiveBookmarksLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        import plone.restapi
+        self.loadZCML(package=plone.restapi)
+        self.loadZCML(package=collective.bookmarks)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.bookmarks:default')
+
+
+COLLECTIVE_BOOKMARKS_FIXTURE = CollectiveBookmarksLayer()
+
+
+COLLECTIVE_BOOKMARKS_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(COLLECTIVE_BOOKMARKS_FIXTURE,),
+    name='CollectiveBookmarksLayer:IntegrationTesting',
+)
+
+
+COLLECTIVE_BOOKMARKS_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(COLLECTIVE_BOOKMARKS_FIXTURE,),
+    name='CollectiveBookmarksLayer:FunctionalTesting',
+)
+
+
+COLLECTIVE_BOOKMARKS_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        COLLECTIVE_BOOKMARKS_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE,
+    ),
+    name='CollectiveBookmarksLayer:AcceptanceTesting',
+)
