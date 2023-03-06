ff74d3367c2be434ddd8436cececb7e24f952319
Katja Süss <k.suess@rohberg.ch>
2023-03-06 14:45:05 +0100

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.bookmarks",
-    version="1.1",
+    version="1.2.dev0",
     description="Bookmarks/ favorites (internal) for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
